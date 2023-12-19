0a49f914972569e6259d6769a2c5935e9bd68cee
Kimo Knowles <kimo.knowles@day8.com.au>
2023-12-19 19:14:31 +0100

    [broken] Use a reaction in a form-2 component

--- src/re_frame_base/views.cljs
+++ src/re_frame_base/views.cljs
@@ -4,12 +4,19 @@
    [re-frame-base.subs :as subs]
    [reagent.core :as r]))
 
-(defn repro []
-  [:div "inspect me!"])
+(defn good []
+  (r/with-let [a (r/atom "good")]
+    [:div "I'm " @a "!"]))
+
+(defn bad []
+  (let [a (r/atom "bad")]
+    (fn []
+      [:div "I'm " @a "!"])))
 
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])]
     [:div
      [:h1
       "Hello from " @name]
-     [repro]]))
+     [good]
+     [bad]]))
